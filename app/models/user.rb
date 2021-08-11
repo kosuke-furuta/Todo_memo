ebe54c1c68a15df0fbd7c79b1c6244cb24600ccd
kosuke-furuta <furu.21.22@gmail.com>
2021-08-12 07:13:20 +0900

    User sign-up in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum:255 },
               uniqueness: true
   has_secure_password
-validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }
 end
--- db/migrate/20210809220612_add_image_name_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddImageNameToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :images_name, :string
-  end
-end
