356b87239cd474c9c1b3680588fe8ce46f1d4618
kosuke-furuta <furu.21.22@gmail.com>
2021-08-15 18:50:49 +0900

    Implement basic login

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -19,7 +19,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('bootstrap/dist/js/bootstrap.min.js')
+require("bootstrap")
 
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
