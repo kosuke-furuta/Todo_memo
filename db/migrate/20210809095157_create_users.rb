54580f04e0b1e075e4675ee00535c7dc45189935
kosuke-furuta <furu.21.22@gmail.com>
2021-10-17 00:32:02 +0900

    db設定変更

--- db/migrate/20210809095157_create_users.rb
+++ db/migrate/20210809095157_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
-      t.string :name, limit: 191
+      t.string :name, limit: 30
       t.string :email, limit: 191
 
       t.timestamps
--- db/migrate/20210921122150_create_active_storage_tables.active_storage.rb
+++ db/migrate/20210921122150_create_active_storage_tables.active_storage.rb
@@ -2,22 +2,22 @@
 class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
   def change
     create_table :active_storage_blobs do |t|
-      t.string   :key,          null: false
-      t.string   :filename,     null: false
-      t.string   :content_type
-      t.text     :metadata
-      t.string   :service_name, null: false
-      t.bigint   :byte_size,    null: false
-      t.string   :checksum,     null: false
-      t.datetime :created_at,   null: false
+      t.string   :key, null: false, limit: 191
+      t.string   :filename, null: false, limit: 191
+      t.string   :content_type, limit: 191
+      t.text     :metadata, limit: 191
+      t.string   :service_name, null: false, limit: 191
+      t.bigint   :byte_size, null: false
+      t.string   :checksum, null: false, limit: 191
+      t.datetime :created_at, null: false
 
       t.index [ :key ], unique: true
     end
 
     create_table :active_storage_attachments do |t|
-      t.string     :name,     null: false
-      t.references :record,   null: false, polymorphic: true, index: false
-      t.references :blob,     null: false
+      t.string     :name, null: false, limit: 191
+      t.references :record, null: false, polymorphic: true, index: false
+      t.references :blob, null: false
 
       t.datetime :created_at, null: false
 
@@ -27,7 +27,7 @@ class CreateActiveStorageTables < ActiveRecord::Migration[5.2]
 
     create_table :active_storage_variant_records do |t|
       t.belongs_to :blob, null: false, index: false
-      t.string :variation_digest, null: false
+      t.string :variation_digest, null: false, limit: 191
 
       t.index %i[ blob_id variation_digest ], name: "index_active_storage_variant_records_uniqueness", unique: true
       t.foreign_key :active_storage_blobs, column: :blob_id
